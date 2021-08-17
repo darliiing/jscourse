a6f6af379a1fe9c8cd9f106822788a8ce0befb47
Daria <example@mail.com>
2021-08-17 11:45:28 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+let numb = 4.5;
+
+console.log(-4/0);
+console.log('srting' * 9);
+
+const persone = 'Alex';
+
+const bool = true;
+const bbol2 = false;
+
+
+let und;
+console.log(und);
+
+
+const obj = {
+    name: 'Dasha',
+    age: 29,
+    isMarried: true
+};
+
+//console.log(obj.age);
+console.log(obj['age']);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.png']
+console.log(arr[0]);
+
+
+const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фам?', '');
+// answers[2] = prompt('Как ваше возр?', '');
+
+console.log(typeof(answers));
+
+
+const name = 'Dasha';
+alert(`Hi ${name}`);
+
+
+
+
+
